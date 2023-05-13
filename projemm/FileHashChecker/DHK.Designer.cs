d1c4e7b9f136f4fe6556f5b1bcab4bd13a90f29b
Burakcan54 <130197342+Burakcan54@users.noreply.github.com>
2023-05-13 18:28:41 +0300

    projem
    projem

---
+++ projemm/FileHashChecker/DHK.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace FileHashChecker
+{
+    partial class DHK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DHK));
+            this.Dosya = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Sec = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Karsilastir = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Dosya
+            // 
+            this.Dosya.SupportMultiDottedExtensions = true;
+            this.Dosya.Title = "DHK DSYSeç";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dosya Seç";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(15, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(534, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Sec
+            // 
+            this.Sec.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sec.Location = new System.Drawing.Point(555, 26);
+            this.Sec.Name = "Sec";
+            this.Sec.Size = new System.Drawing.Size(131, 26);
+            this.Sec.TabIndex = 1;
+            this.Sec.Text = "Seç";
+            this.Sec.UseVisualStyleBackColor = true;
+            this.Sec.Click += new System.EventHandler(this.Sec_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Dosya Adı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(106, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "N/A";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Dosyanın MD5 Değeri";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(15, 172);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(596, 26);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "MD5 Hash Değerini Giriniz";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(15, 276);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(534, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Karsilastir
+            // 
+            this.Karsilastir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Karsilastir.Location = new System.Drawing.Point(555, 276);
+            this.Karsilastir.Name = "Karsilastir";
+            this.Karsilastir.Size = new System.Drawing.Size(131, 26);
+            this.Karsilastir.TabIndex = 6;
+            this.Karsilastir.Text = "Karşılaştır";
+            this.Karsilastir.UseVisualStyleBackColor = true;
+            this.Karsilastir.Click += new System.EventHandler(this.Karsilastir_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(255, 317);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 23);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Dosya Orijinal";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(242, 340);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(166, 23);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Dosya Orijinal Değil";
+            this.label7.Visible = false;
+            // 
+            // DHK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 373);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Sec);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Karsilastir);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "DHK";
+            this.Text = "Dosya Hash Kontrolcüsü";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog Dosya;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Sec;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Karsilastir;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ projemm/FileHashChecker/DHK.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Security.Policy;
+
+
+namespace FileHashChecker
+{
+    public partial class DHK : Form
+    {
+        public DHK()
+        {
+            InitializeComponent();
+        }
+        public static class Algorithms
+        {
+            public static readonly HashAlgorithm MD5 = new MD5CryptoServiceProvider();
+        }
+        public static string GetHashFromFile(string fileName, HashAlgorithm algorithm)
+        {
+            using (var stream = new BufferedStream(File.OpenRead(fileName), 100000))
+            {
+                return BitConverter.ToString(algorithm.ComputeHash(stream)).Replace("-", string.Empty).ToLowerInvariant();
+            }
+        }
+        private void Sec_Click(object sender, EventArgs e)
+        {
+            if(Dosya.ShowDialog() == DialogResult.OK)
+            {
+                string Yol = Dosya.FileName;
+                textBox1.Text = Yol;
+                if (File.Exists(Yol)) 
+                {
+                    string dosyaAd = Dosya.SafeFileName;
+                    FileInfo fileInfo = new FileInfo(Yol);
+                    label3.Text = dosyaAd;
+                }
+                string Md5 = GetHashFromFile(Yol, Algorithms.MD5);
+                textBox2.Text = Md5;
+            }
+        }
+        private void Karsilastir_Click(object sender, EventArgs e)
+        {
+            if( textBox3.Text == textBox2.Text)
+            {
+                label6.Visible = true;
+                label7.Visible = false;
+            }
+            else 
+            {
+                label6.Visible = false;
+                label7.Visible= true;
+            }
+        }
+    }
+}
